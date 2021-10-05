7ba8301695e17d8fb19cd39003de83e2c66b344f
Yasmine ESSAFRAOUY <essafraouyyasmine@gmail.com>
2021-10-05 18:28:15 +0100

    second commit

--- changementClim1/settings.py
+++ changementClim1/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'Mydatabase',
         'USER' : 'postgres',
-        'PASSWORD' : 'yaya@123',
+        'PASSWORD' : 'password',
         'HOST' : 'localhost'
     }
 }
@@ -140,7 +140,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='email'
+EMAIL_HOST_USER ='essafraouyyasmine@gmail.com'
 EMAIL_HOST_PASSWORD = 'password'
 
 # Default primary key field type
